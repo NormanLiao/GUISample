a84bf507488884ade195acad1eecddc0f12df920
norman.liao <norman.liao@style.me>
2018-11-02 12:29:38 +0800

    Add Phong shading.

--- Renderer/Viewer.cpp
+++ Renderer/Viewer.cpp
@@ -44,11 +44,7 @@ RenderView::RenderView() : nanogui::Screen(Eigen::Vector2i(1024, 768), "Render V
 	m_shader.init(
 		/* An identifying name */
 		"Basis Shader",
-
-		/* Vertex shader */
-		prefab->mat.m_shader.m_vertex_shader
-		,
-		/* Fragment shader */
+		prefab->mat.m_shader.m_vertex_shader ,
 		prefab->mat.m_shader.m_fragment_shader
 	);
 
@@ -64,10 +60,15 @@ RenderView::RenderView() : nanogui::Screen(Eigen::Vector2i(1024, 768), "Render V
 		positions.col(idx) << prefab->geo.m_pos[idx].x, prefab->geo.m_pos[idx].y, prefab->geo.m_pos[idx].z;
 	}
 	
+	nanogui::MatrixXf normals(3, prefab->geo.m_normals.size());
+	for (unsigned int idx = 0; idx< prefab->geo.m_normals.size(); idx++)
+	{
+		normals.col(idx) << prefab->geo.m_normals[idx].x, prefab->geo.m_normals[idx].y, prefab->geo.m_normals[idx].z;
+	}
 	m_shader.bind();
 	m_shader.uploadIndices(indices);
-	m_shader.uploadAttrib("position", positions);
-	m_shader.setUniform("intensity", 1.0f);
+	m_shader.uploadAttrib("VertexPosition", positions);
+	m_shader.uploadAttrib("VertexNormal", normals);
 	setScene();
 }
 
@@ -83,8 +84,26 @@ void RenderView::setScene()
 	Eigen::Matrix4f view = transferGLMtoEigen(glm::lookAt(glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
 	Eigen::Matrix4f projection = transferGLMtoEigen(glm::perspective(45.0f, (float)4 / 3, 0.01f, 12.0f));
 	Eigen::Matrix4f mvp = projection * view * model;
+	Eigen::Matrix4f mv = view * model;
+	
 
-	m_shader.setUniform("modelViewProj", mvp);
+	m_shader.setUniform("MVP", mvp);
+	m_shader.setUniform("ModelViewMatrix", mv);
+	m_shader.setUniform("NormalMatrix", Eigen::Matrix3f(mv.block(0,0,3,3)));
+
+	Eigen::Matrix4f light_pos;
+	light_pos.setIdentity();
+	light_pos = view * light_pos;
+
+	m_shader.setUniform("Material.Kd", Eigen::Vector3f(0.9f, 0.5f, 0.3f));
+	m_shader.setUniform("Light.Ld", Eigen::Vector3f(1.0f, 1.0f, 1.0f));
+	m_shader.setUniform("Light.Position", light_pos);
+	m_shader.setUniform("Material.Ka", Eigen::Vector3f(0.9f, 0.5f, 0.3f));
+	m_shader.setUniform("Light.La", Eigen::Vector3f(0.8f, 0.8f, 0.8f));
+	m_shader.setUniform("Material.Ks", Eigen::Vector3f(0.9f, 0.5f, 0.3f));
+	m_shader.setUniform("Light.Ls", Eigen::Vector3f(1.0f, 1.0f, 1.0f));
+	m_shader.setUniform("Material.Shininess", 100.0f);
+	
 }
 
 void RenderView::setObject()
@@ -95,8 +114,8 @@ bool RenderView::loadObject(std::string filepath)
 {
 	Prefab *prefab = new Prefab();
 	prefab->loadObj(filepath);
-	prefab->mat.m_shader.m_vertex_shader = prefab->loadShaderFile("../resources/shaders/basis.vert");
-	prefab->mat.m_shader.m_fragment_shader = prefab->loadShaderFile("../resources/shaders/basis.frag");
+	prefab->mat.m_shader.m_vertex_shader = prefab->loadShaderFile("../resources/shaders/phong.vert");
+	prefab->mat.m_shader.m_fragment_shader = prefab->loadShaderFile("../resources/shaders/phong.frag");
 	m_prefabs.push_back(prefab);
 	return true;
 }
